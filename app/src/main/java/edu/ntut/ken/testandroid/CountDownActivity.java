5dba1b7cf5c38e84183f808f07d6b7ef32a2b716
KnTa <a25351472@hotmail.com>
2019-01-17 12:35:12 +0800

    add count down page

---
+++ app/src/main/java/edu/ntut/ken/testandroid/CountDownActivity.java
@@ -0,0 +1,27 @@
+package edu.ntut.ken.testandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CountDownActivity extends AppCompatActivity {
+
+    Button btnCancelCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count_down);
+
+        btnCancelCount = (Button) findViewById(R.id.btnCancelCount);
+        btnCancelCount.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        finish();
+                    }
+                }
+        );
+    }
+}
--- app/src/main/java/edu/ntut/ken/testandroid/MainActivity.java
+++ app/src/main/java/edu/ntut/ken/testandroid/MainActivity.java
@@ -1,13 +1,34 @@
 package edu.ntut.ken.testandroid;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNextToCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNextToCount=(Button) findViewById(R.id.btnNextToCount);
+        btnNextToCount.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        launchCountActivity();
+                    }
+                }
+        );
+    }
+
+    private void launchCountActivity(){
+        Intent intent = new Intent(this, CountDownActivity.class);
+        startActivity(intent);
     }
 }
